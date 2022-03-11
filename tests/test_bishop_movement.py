f8941ac5020b9c677ccd3fd11e37f99ca6fddc89
Andy Wu <andywurocks123@gmail.com>
2022-03-11 16:56:04 -0500

    adjusted test formatting

--- tests/test_bishop_movement.py
+++ tests/test_bishop_movement.py
@@ -6,46 +6,48 @@ class TestSum(unittest.TestCase):
 
     def test_bishop_no_movement(self):
         board = Board()
-        bishop1 = Bishop("W", 7, 0, board)
-        board.add_piece(bishop1, 7, 0)
 
+        bishop1 = Bishop("W", 7, 0, board)
         bishop2 = Bishop("W", 6, 1, board)
+
+        board.add_piece(bishop1, 7, 0)
         board.add_piece(bishop2, 6, 1)
 
         self.assertEqual(bishop1.generate_legal_moves(), [], "Should be empty array")
 
     def test_bishop_capture(self):
         board = Board()
-        bishop1 = Bishop("W", 7, 0, board)
-        board.add_piece(bishop1, 7, 0)
 
+        bishop1 = Bishop("W", 7, 0, board)
         bishop2 = Bishop("B", 6, 1, board)
+
+        board.add_piece(bishop1, 7, 0)
         board.add_piece(bishop2, 6, 1)
 
         self.assertEqual(bishop1.generate_legal_moves(), [(6, 1)], "Should be [(6, 1)]")
     
     def test_bishop_capture_and_move(self):
         board = Board()
+
         bishop1 = Bishop("W", 6, 1, board)
-        board.add_piece(bishop1, 6, 1)
-
         bishop2 = Bishop("B", 7, 0, board)
-        board.add_piece(bishop2, 7, 0)
-
         bishop3 = Bishop("W", 5, 2, board)
-        board.add_piece(bishop3, 5, 2)
-
         bishop4 = Bishop("W", 5, 0, board)
-        board.add_piece(bishop4, 5, 0)
-
         bishop5 = Bishop("W", 7, 2, board)
+
+        board.add_piece(bishop1, 6, 1)
+        board.add_piece(bishop2, 7, 0)
+        board.add_piece(bishop3, 5, 2)
+        board.add_piece(bishop4, 5, 0)
         board.add_piece(bishop5, 7, 2)
 
         self.assertEqual(bishop1.generate_legal_moves(), [(7, 0)], "Should be [(7, 0)]")
 
     def test_full_diagonal(self):
         board = Board()
+
         bishop1 = Bishop("W", 7, 7, board)
+        
         board.add_piece(bishop1, 7, 7)
 
         self.assertEqual(bishop1.generate_legal_moves(), 
--- tests/test_queen_movement.py
+++ tests/test_queen_movement.py
@@ -6,16 +6,15 @@ class TestSum(unittest.TestCase):
 
     def test_queen_no_movement(self):
         board = Board()
+
         queen1 = Queen("W", 7, 0, board)
-        board.add_piece(queen1, 7, 0)
-
         queen2 = Queen("W", 6, 1, board)
-        board.add_piece(queen2, 6, 1)
-
         queen3 = Queen("W", 6, 0, board)
-        board.add_piece(queen3, 6, 0)
-
         queen4 = Queen("W", 7, 1, board)
+
+        board.add_piece(queen1, 7, 0)
+        board.add_piece(queen2, 6, 1)
+        board.add_piece(queen3, 6, 0)
         board.add_piece(queen4, 7, 1)
 
         self.assertEqual(queen1.generate_legal_moves(), [], "Should be empty array")
@@ -24,15 +23,13 @@ class TestSum(unittest.TestCase):
         board = Board()
 
         queen1 = Queen("W", 7, 0, board)
-        board.add_piece(queen1, 7, 0)
-
         queen2 = Queen("B", 6, 1, board)
-        board.add_piece(queen2, 6, 1)
-
         queen3 = Queen("B", 6, 0, board)
-        board.add_piece(queen3, 6, 0)
-
         queen4 = Queen("W", 7, 1, board)
+        
+        board.add_piece(queen1, 7, 0)
+        board.add_piece(queen2, 6, 1)
+        board.add_piece(queen3, 6, 0)
         board.add_piece(queen4, 7, 1)
 
         self.assertEqual(queen1.generate_legal_moves(), [(6, 1), (6, 0)],
@@ -42,20 +39,35 @@ class TestSum(unittest.TestCase):
         board = Board()
 
         queen1 = Queen("W", 7, 3, board)
-        board.add_piece(queen1, 7, 3)
-
         queen2 = Queen("W", 6, 3, board)
-        board.add_piece(queen2, 6, 3)
-
         queen3 = Queen("W", 6, 2, board)
-        board.add_piece(queen3, 6, 2)
-
         queen4 = Queen("W", 6, 4, board)
-        board.add_piece(queen4, 6, 4)
 
+        board.add_piece(queen1, 7, 3)
+        board.add_piece(queen2, 6, 3)
+        board.add_piece(queen3, 6, 2)
+        board.add_piece(queen4, 6, 4)
+        
         self.assertEqual(queen1.generate_legal_moves(), 
         [(7, 4), (7, 5), (7, 6), (7, 7), (7, 2), (7, 1), (7, 0)],
         "Should be [(7, 4), (7, 5), (7, 6), (7, 7), (7, 2), (7, 1), (7, 0)]")
+
+    def test_vertical_movement(self):
+        board = Board()
+        
+        queen1 = Queen("W", 4, 0, board)
+        queen2 = Queen("W", 5, 1, board)
+        queen3 = Queen("W", 4, 1, board)
+        queen4 = Queen("W", 3, 1, board)
+
+        board.add_piece(queen1, 4, 0)
+        board.add_piece(queen2, 5, 1)
+        board.add_piece(queen3, 4, 1)
+        board.add_piece(queen4, 3, 1)
+
+        self.assertEqual(queen1.generate_legal_moves(), 
+        [(5, 0), (6, 0), (7, 0), (3, 0), (2, 0), (1, 0), (0, 0)],
+        "Should be [(5, 0), (6, 0), (7, 0), (3, 0), (2, 0), (1, 0), (0, 0)]")
 
 
 if __name__ == '__main__':
