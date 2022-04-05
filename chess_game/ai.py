a1f6405f228929f1492708a42aeea8ed22652e82
Andy Wu <andywurocks123@gmail.com>
2022-04-05 13:40:11 -0400

    negamax

--- chess_game/ai.py
+++ chess_game/ai.py
@@ -18,52 +18,31 @@ def get_best_move(moves, game_state, board, turn):
 def minimax(moves, game_state, board, turn, depth):
     global next_move
     #always assumes opponent plays the best move
+    multiplier = 1 if turn == "W" else -1
     if depth == 0:
-        return evaluate(game_state)
+        return multiplier * evaluate(game_state)
 
-    if turn == "W":
-        max_score = -math.inf
-        for move in moves:
-            start_row, start_column = move[0], move[1]
-            end_row, end_column = move[2], move[3]
-            piece = board.get_square(start_row, start_column)
-            game_state.log_move(piece, start_row, start_column, end_row, end_column)
+    max_score = -math.inf
+    for move in moves:
+        start_row, start_column = move[0], move[1]
+        end_row, end_column = move[2], move[3]
+        piece = board.get_square(start_row, start_column)
+        game_state.log_move(piece, start_row, start_column, end_row, end_column)
 
-            prev_move = game_state.get_prev_move()
-            color = game_state.get_turn()
-            legal_moves = board.get_all_legal_moves(prev_move, color)
-            score = minimax(legal_moves, game_state, board, color, depth-1)
+        prev_move = game_state.get_prev_move()
+        color = game_state.get_turn()
+        legal_moves = board.get_all_legal_moves(prev_move, color)
+        score = -minimax(legal_moves, game_state, board, color, depth-1)
 
-            if score >= max_score:
-                max_score = score
-                if depth == DEPTH:
-                    next_move = move
+        if score >= max_score:
+            max_score = score
+            if depth == DEPTH:
+                next_move = move
 
-            game_state.undo_move()
+        game_state.undo_move()
 
-        return max_score
-    
-    else:
-        min_score = math.inf
-        for move in moves:
-            start_row, start_column = move[0], move[1]
-            end_row, end_column = move[2], move[3]
-            piece = board.get_square(start_row, start_column)
-            game_state.log_move(piece, start_row, start_column, end_row, end_column)
-
-            prev_move = game_state.get_prev_move()
-            color = game_state.get_turn()
-            legal_moves = board.get_all_legal_moves(prev_move, color)
-            score = minimax(legal_moves, game_state, board, color, depth-1)
-
-            if score <= min_score:
-                min_score = score
-                if depth == DEPTH:
-                    next_move = move
-
-            game_state.undo_move()
-
-        return min_score
+    print(max_score)
+    return max_score
 
 def evaluate(game_state):
     multiplier = -1 if game_state.get_turn() == "W" else 1
